ad8eb6ca0dee9b8d56e82aedd7d85a7ad464dde8
Sylvia Maguiña <smag23@gmail.com>
2017-12-07 15:12:19 -0800

    Import props, template strings and list item keys

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDcSKBBG28vrs5sgS_DZpKmPzvI8NwpVv4';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {console.log(data);
+});
 
-// Create a new component. This component should produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
